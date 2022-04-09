2612225f1fb505a714f26f1fbd04fa8faec02798
Egoist <epsilonelittr@gmail.com>
2022-04-10 02:26:23 +0300

    Add section title bar component

---
+++ resources/views/web/components/common/section-title-bar.blade.php
@@ -0,0 +1,33 @@
+<!-- Section Title Bar Container -->
+<div class="section-title-bar-container">
+    <!-- Title Bar -->
+    <div class="title-bar">
+        <!-- Title -->
+        <h1 class="title">
+            {{ $title ?? 'Title' }}
+        </h1>
+
+        <!-- Short Message -->
+        <h3 class="short-message">
+            {{ $message ?? '' }}
+        </h3>
+
+        <!-- Underline -->
+        <div class="underline line-orange">
+            <!-- Line 1 -->
+            <span class="line line-1"></span>
+
+            <!-- Line 2 -->
+            <span class="line line-2"></span>
+
+            <!-- Line 3 -->
+            <span class="line line-3"></span>
+
+            <!-- Line 4 -->
+            <span class="line line-4"></span>
+
+            <!-- Line 5 -->
+            <span class="line line-5"></span>
+        </div>
+    </div>
+</div>
