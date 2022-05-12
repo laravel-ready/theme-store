86f0b60f2afa2d5d552a63930576720e54b96890
Egoist <epsilonelittr@gmail.com>
2022-05-12 16:33:22 +0300

    Reformat pagination component

--- resources/views/web/components/common/pagination.blade.php
+++ resources/views/web/components/common/pagination.blade.php
@@ -55,7 +55,8 @@
             <div class="number-wrapper">
                 <!-- Number Container -->
                 <span class="number-container">
-                    {{-- Previous Page Link --}} @if ($paginator->onFirstPage())
+                    {{-- Previous Page Link --}}
+                    @if ($paginator->onFirstPage())
                         <span class="class-span-7" aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
                             <!-- Previous Page Content -->
                             <span class="previous-page-content" aria-hidden="true">
@@ -76,55 +77,64 @@
                                     clip-rule="evenodd"></path>
                             </svg>
                         </a>
-                        @endif {{-- Pagination Elements --}} @foreach ($elements as $element)
-                            {{-- "Three Dots" Separator --}} @if (is_string($element))
-                                <span class="class-span-9" aria-disabled="true">
-                                    <!-- Three Dots Separator -->
-                                    <span class="three-dots-separator">
-                                        {{ $element }}
-                                    </span>
+                    @endif
+
+                    {{-- Pagination Elements --}}
+                    @foreach ($elements as $element)
+                        {{-- "Three Dots" Separator --}}
+                        @if (is_string($element))
+                            <span class="class-span-9" aria-disabled="true">
+                                <!-- Three Dots Separator -->
+                                <span class="three-dots-separator">
+                                    {{ $element }}
                                 </span>
-                                @endif {{-- Array Of Links --}} @if (is_array($element))
-                                    @foreach ($element as $page => $url)
-                                        @if ($page == $paginator->currentPage())
-                                            <span class="class-span-10" aria-current="page">
-                                                <!-- Current Page -->
-                                                <span class="current-page">
-                                                    {{ $page }}
-                                                </span>
-                                            </span>
-                                        @else
-                                            <!-- Link -->
-                                            <a class="link" href="{{ $url }}"
-                                                aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
-                                                {{ $page }}
-                                            </a>
-                                        @endif
-                                    @endforeach
-                                @endif
-                                @endforeach {{-- Next Page Link --}} @if ($paginator->hasMorePages())
-                                    <!-- Next Page Link -->
-                                    <a class="next-page-link" href="{{ $paginator->nextPageUrl() }}" rel="next"
-                                        aria-label="{{ __('pagination.next') }}">
-                                        <svg fill="currentColor" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd"
-                                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                                clip-rule="evenodd"></path>
-                                        </svg>
-                                    </a>
-                                @else
-                                    <span class="class-span-13" aria-disabled="true"
-                                        aria-label="{{ __('pagination.next') }}">
-                                        <!-- Next Page Content -->
-                                        <span class="next-page-content" aria-hidden="true">
-                                            <svg fill="currentColor" viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd"
-                                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                                    clip-rule="evenodd"></path>
-                                            </svg>
+                            </span>
+                        @endif
+
+                        {{-- Array Of Links --}}
+                        @if (is_array($element))
+                            @foreach ($element as $page => $url)
+                                @if ($page == $paginator->currentPage())
+                                    <span class="class-span-10" aria-current="page">
+                                        <!-- Current Page -->
+                                        <span class="current-page">
+                                            {{ $page }}
                                         </span>
                                     </span>
+                                @else
+                                    <!-- Link -->
+                                    <a class="link" href="{{ $url }}"
+                                        aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
+                                        {{ $page }}
+                                    </a>
                                 @endif
+                            @endforeach
+                        @endif
+                    @endforeach
+
+                    {{-- Next Page Link --}}
+                    @if ($paginator->hasMorePages())
+                        <!-- Next Page Link -->
+                        <a class="next-page-link" href="{{ $paginator->nextPageUrl() }}" rel="next"
+                            aria-label="{{ __('pagination.next') }}">
+                            <svg fill="currentColor" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd"
+                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </a>
+                    @else
+                        <span class="class-span-13" aria-disabled="true" aria-label="{{ __('pagination.next') }}">
+                            <!-- Next Page Content -->
+                            <span class="next-page-content" aria-hidden="true">
+                                <svg fill="currentColor" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </span>
+                        </span>
+                    @endif
                 </span>
             </div>
         </div>
